0f92260cd3d7c4e8987d8a4918fed1e1aef2351f
aayushs-edu <70823635+aayushs-edu@users.noreply.github.com>
2024-02-03 09:14:40 -0800

    faeofea

---
+++ main.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+batch_size = 32
+img_height = 100
+img_width = 100
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  './DATASET/TRAIN',
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+  './DATASET/TEST',
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  './DATASET/TRAIN',
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+
+class_names = train_ds.class_names
+print(class_names)
+
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_ds.take(1):
+#   for i in range(9):
+#     ax = plt.subplot(3, 3, i + 1)
+#     plt.imshow(images[i].numpy().astype("uint8"))
+#     plt.title(class_names[labels[i]])
+#     plt.axis("off")
+
+# plt.show()
+
+# Normalize pixel values (0 -> 1)
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+# Configure dataset for performance
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+num_classes = 5
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(128, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+
+  tf.keras.layers.Dense(128, activation='sigmoid'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=10
+)
+
+print("Fit model on training data")
+
+print("Evaluate on test data")
+results = model.evaluate(test_ds, batch_size=128)
+print("test loss, test acc:", results)
